1ea2659c86d97039f38d9f8b15996ebb1a56cfdc
HP <HP@DIAMONDN>
2023-10-04 15:11:02 +0700

    Add project files.

---
+++ Models/Course.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TH_04Oct.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
