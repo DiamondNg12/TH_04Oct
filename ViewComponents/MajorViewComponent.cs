0e177593c4bb33b534464b953a7b3b9d50b4b099
diamondn <diamond12.ng@gmail.com>
2023-10-12 16:32:12 +0700

    lab4 + half lab5

---
+++ ViewComponents/MajorViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyWebApp.Data;
+using TH_04Oct.Models;
+
+namespace TH_04Oct.ViewComponents
+{
+    public class MajorViewComponent : ViewComponent
+    {
+        SchoolContext db;
+        List<Major> majors;
+
+        public MajorViewComponent (SchoolContext _context)
+        {
+            db = _context;
+            majors = db.Majors.ToList();
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("RenderMajor", majors);
+        }
+    }
+}
